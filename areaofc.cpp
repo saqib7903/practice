7072a752dbee8325575a08005d04f43d18acbe9c
saqib7903 <132329707+saqib7903@users.noreply.github.com>
2023-05-09 22:52:41 +0530

    Create areaofc.cpp

---
+++ areaofc.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int radius,area,circumference;
+cout<<"Enter Radius"<<endl;
+cin>>radius;
+area = 3.14*radius*radius;
+cout<<area<<endl;
+circumference = 2*3.14*radius;
+cout<<circumference;
+return 0;
+
+}
+
