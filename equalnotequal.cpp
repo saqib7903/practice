c20189c1dfef81d35e01aad7473506a228dc1417
saqib7903 <132329707+saqib7903@users.noreply.github.com>
2023-05-10 00:16:09 +0530

    Create equalnotequal.cpp

---
+++ equalnotequal.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int num1 , num2 ;
+    cout<<"enter number:"<<endl;
+    cin>>num1>>num2;
+    if (num1 == num2) {
+        cout<<"equal";
+      
+    }   else {
+            cout<<"not equal";
+        }
+    return 0;
+    }
